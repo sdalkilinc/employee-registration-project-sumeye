17502f4b77210aecace3e1ddadd5a0d27052b66d
Sumeye <dalkilincsumeye@gmail.com>
2022-10-24 16:11:18 -0400

    Initial commit

---
+++ src/main/java/com/cydeo/EmployeeRegistrationProjectSumeyeApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeRegistrationProjectSumeyeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeRegistrationProjectSumeyeApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/cydeo/model/Employee.java
@@ -0,0 +1,25 @@
+package com.cydeo.model;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Employee {
+
+    private String firstName;
+    private String lastName;
+    private LocalDate birthday;
+    private String email;
+    private String password;
+    private String address;
+    private String address2;
+    private String city;
+    private String state;
+    private String zipCode;
+
+}
