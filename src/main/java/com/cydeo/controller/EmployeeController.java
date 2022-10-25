0d3b1df0390fc1512b2635a31814c77d7c5d9a73
Sumeye <dalkilincsumeye@gmail.com>
2022-10-24 21:12:47 -0400

    More work added

--- src/main/java/com/cydeo/controller/EmployeeController.java
+++ src/main/java/com/cydeo/controller/EmployeeController.java
@@ -1,6 +1,8 @@
 package com.cydeo.controller;
 
+import com.cydeo.model.Employee;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,9 +11,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class EmployeeController {
 
     @GetMapping("/register")
-    public String createEmployee(){
+    public String createEmployee(Model model){
+
+        model.addAttribute("employee", new Employee());
+
         return "employee/employee-create";
     }
+
+
 
 
 }
