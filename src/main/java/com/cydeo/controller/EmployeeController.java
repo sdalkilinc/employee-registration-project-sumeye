787b0b020a8900d5aadc81a845933b7c6fb4948d
Sumeye <dalkilincsumeye@gmail.com>
2022-10-26 12:14:54 -0400

    More work added

--- src/main/java/com/cydeo/controller/EmployeeController.java
+++ src/main/java/com/cydeo/controller/EmployeeController.java
@@ -1,5 +1,6 @@
 package com.cydeo.controller;
 
+import com.cydeo.bootstrap.DataGenerator;
 import com.cydeo.model.Employee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +15,7 @@ public class EmployeeController {
     public String createEmployee(Model model){
 
         model.addAttribute("employee", new Employee());
+        model.addAttribute("stateList", DataGenerator.getAllStates());
 
         return "employee/employee-create";
     }
