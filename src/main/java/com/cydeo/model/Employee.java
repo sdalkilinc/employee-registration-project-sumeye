01d821260cfb83f792a66077978a352b1a56e150
Sumeye <dalkilincsumeye@gmail.com>
2022-10-26 20:11:10 -0400

    More work added

--- src/main/java/com/cydeo/model/Employee.java
+++ src/main/java/com/cydeo/model/Employee.java
@@ -3,6 +3,10 @@ package com.cydeo.model;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Setter
@@ -12,6 +16,17 @@ import java.time.LocalDate;
 @ToString
 public class Employee {
 
+
+//    @NotNull  Field should not be null
+//    @NotEmpty Field should not be ""
+//    @NotBlank Field should not be "          "
+//
+    //@NotNull -> @NotNull
+    //@NotEmpty -> @NotNull + @NotEmpty
+    //@NotBlank -> @NotNull + @NotEmpty + @NotBlank
+
+    @NotBlank
+    @Size (max = 12, min = 2)
     private String firstName;
     private String lastName;
 
